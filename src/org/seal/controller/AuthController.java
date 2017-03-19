b60bf2c30f231cd6cfa8ce6351d668c2e41b0a9a
Jason Jover <jason.jover@iacademy.edu.ph>
2017-03-20 06:45:11 +0800

    Added initial mongodb login methods

--- src/org/seal/controller/AuthController.java
+++ src/org/seal/controller/AuthController.java
@@ -1,5 +1,19 @@
 package org.seal.controller;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.mongodb.morphia.Datastore;
+import org.seal.model.ResponseEntity;
+import org.seal.model.User;
+import org.seal.util.MongoDriver;
+import org.seal.util.UserManager;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,6 +24,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
 
-	
+	// login
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ResponseEntity create(@RequestBody User user) {
+
+		UserManager userM = new UserManager();
+		
+		User loggedUser = userM.login(user.getUsername(), user.getPassword());
+		List<User> data = new ArrayList<User>();
+		
+		data.add(loggedUser);
+		
+		// we need validation
+		
+		ResponseEntity response;
+		
+		response = new ResponseEntity(data, 200, "Successful Creation");
+		return response;
+	}
 	
 }
--- src/org/seal/util/UserManager.java
+++ src/org/seal/util/UserManager.java
@@ -1,5 +1,7 @@
 package org.seal.util;
 
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.query.Query;
 import org.seal.model.User;
 
 /**
@@ -8,7 +10,14 @@ import org.seal.model.User;
  * This class will also be used for any changes needed to be done on the user on code run.
  *
  */
-public class UserManager { 
+public class UserManager {
+	
+	private Datastore ds;
+	
+	public UserManager() {
+		ds = MongoDriver.getConnection();
+//		not sure if will need a singleton or if this is okay.
+	}
 	
 	/**
 	 * This method is used to clear passwords after auth (like logins).
@@ -18,6 +27,14 @@ public class UserManager {
 		user.setPassword(null);
 		
 		return user;
+	}
+	
+	public User login(String user, String pass) {
+		
+		Query<User> query = ds.createQuery(User.class);
+		User retrieved = query.filter("username ==", user).get();
+				
+		return null;
 	}
 	
 	
