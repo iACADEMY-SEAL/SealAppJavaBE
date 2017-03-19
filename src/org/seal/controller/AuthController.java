1655c3a1520d84374d480e1074d6bd3230bcdca6
Jason Jover <jason.jover@iacademy.edu.ph>
2017-03-20 05:58:16 +0800

    Added Auth Controller and fixed model creation date for achievements

---
+++ src/org/seal/controller/AuthController.java
@@ -0,0 +1,15 @@
+package org.seal.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Jason Jover
+ * This controller is responsible for logging in, session tracking and management, and logout.
+ * 
+ */
+@RestController
+public class AuthController {
+
+	
+	
+}
--- src/org/seal/model/Achievement.java
+++ src/org/seal/model/Achievement.java
@@ -1,8 +1,12 @@
 package org.seal.model;
 
+import java.sql.Date;
+
 public class Achievement {
 	
 	private String achievementName;
+	
+	private Date dateCreated;
 
 	public String getAchievementName() {
 		return achievementName;
@@ -10,6 +14,14 @@ public class Achievement {
 
 	public void setAchievementName(String achievementName) {
 		this.achievementName = achievementName;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
 	}
 	
 	
